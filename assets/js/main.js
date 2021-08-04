a8150eb8acf343bf113fcba0d2790ad0d11adfaa
Sri Harsha Jaddu <42829846+SriHarsha-J99@users.noreply.github.com>
2021-08-04 13:36:32 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const galleryLightbox = GLightbox({
+    selector: '.gallery-lightbox'
+  });
+const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
